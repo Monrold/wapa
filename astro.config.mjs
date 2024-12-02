ddefc9f7a332b194dcd3d7845d0976fbd41f6f64
Monrold <soporte.draw@gmail.com>
2024-12-02 03:03:11 -0600

    fix; deploy in cloudfare ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-
+import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
@@ -8,4 +8,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
+  adapter: cloudflare(),
 });
