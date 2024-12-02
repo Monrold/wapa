9762a37cb228a7a62214cc7325b5814f87dd7646
Monrold <soporte.draw@gmail.com>
2024-12-02 10:22:07 -0600

    fix: cloudfare key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY,
+  import.meta.env.PUBLIC_SUPABASE_URL,
+  import.meta.env.PUBLIC_SUPABASE_ANON_KEY,
 );
+
+console.log("SUPABASE_URL:", import.meta.env.PUBLIC_SUPABASE_URL);
+console.log("SUPABASE_ANON_KEY:", import.meta.env.PUBLIC_SUPABASE_ANON_KEY);
