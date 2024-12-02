bdd8ca64c3f1b828e3b5f5457bcb7803f68c6f2e
Monrold <soporte.draw@gmail.com>
2024-12-02 05:06:57 -0600

    fix; adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: 'server',
-  adapter: cloudflare(),
+  adapter: netlify(),
 });
