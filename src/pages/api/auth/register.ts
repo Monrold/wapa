1793c601481603541a49fb0abc710b8b4fe5de90
Monrold <soporte.draw@gmail.com>
2024-12-06 13:01:37 -0600

    feat: name user

--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -7,14 +7,20 @@ export const POST: APIRoute = async ({ request, redirect }) => {
   const formData = await request.formData();
   const email = formData.get("email")?.toString();
   const password = formData.get("password")?.toString();
+  const fullName = formData.get("full_name")?.toString();
 
-  if (!email || !password) {
-    return new Response("Correo electrónico y contraseña obligatorios", { status: 400 });
+  if (!email || !password || !fullName) {
+    return new Response("Correo electrónico, contraseña y nombre completo son obligatorios", { status: 400 });
   }
 
   const { error } = await supabase.auth.signUp({
     email,
     password,
+    options: {
+      data: {
+        full_name: fullName,  // Envía el nombre completo como parte de los metadatos
+      },
+    },
   });
 
   if (error) {
