d13dd6e8b89f1d48e6df77f7877d57805ed93d88
monrold <soporte.draw@gmail.com>
2024-12-12 15:26:32 -0600

    feat: tienda

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				frank: ['"Frank Ruhl Libre"', "serif"], // Agrega la fuente con un nombre de referencia
+			  },
+		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 }
