80130519d78b02e309f36696e21b9337aa519fda
Monrold <soporte.draw@gmail.com>
2024-12-11 22:53:26 -0600

    feat: blog new

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import mdx from '@astrojs/mdx';
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), mdx()],
   output: 'server',
   adapter: cloudflare(),
 });
